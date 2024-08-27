20ccaed5b81d65bab52444e794823a1c526ce002
Heejae Lee <gmlwo1204@gmail.com>
2024-08-27 23:15:18 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,9 @@
+function solution(array, commands) {
+    var answer = [];
+   
+  commands.forEach(command => {
+  let arr = array.slice(command[0] - 1, command[1]).sort((a, b) => a-b);  
+  answer.push(arr[command[2] - 1])  
+  })
+    return answer;
+}
