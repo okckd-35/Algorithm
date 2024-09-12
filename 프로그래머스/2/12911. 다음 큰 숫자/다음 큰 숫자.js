83f9af0fc2a027499c861021f241f00bc140d23a
Heejae Lee <gmlwo1204@gmail.com>
2024-09-12 18:14:58 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -2,8 +2,8 @@ function solution(n) {
     let answer = n + 1;
 
     while (true) {
-        const firstN = n.toString(2).split("").filter(v => v === "1").length;
-        const nextN = answer.toString(2).split("").filter(v => v === "1").length;
+        const firstN = n.toString(2).split("1").length;
+        const nextN = answer.toString(2).split("1").length;
 
         if (firstN === nextN) {
             break;
