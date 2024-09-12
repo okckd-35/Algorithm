97fae988f51ccb55a34366a06b8ddc6adbc6e172
Heejae Lee <gmlwo1204@gmail.com>
2024-09-12 14:06:03 +0900

    [level 2] Title: 피보나치 수, Time: 6.15 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,7 +1,7 @@
 function solution(n) {
     let F = [0, 1];
     for(let i=2; i<=n; i++) {
-        F[i] = (F[i-1]+F[i-2])%1234567;
+        F[i] = (F[i-2] + F[i-1])%1234567;
     }
     
     return F[n];
